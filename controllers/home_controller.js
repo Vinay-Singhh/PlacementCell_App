9f5824ccfa5c90436ca2cf0490a0720a6c4af352
Vinay-Singhh <vinaysingh@Vinays-MacBook-Air.local>
2022-07-29 01:10:11 +0530

    fixing issues

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,14 +1,33 @@
 const Student = require('../models/student');
 
-module.exports.home = function(req, res){
-    Student.find({}, function(err, students){
-        if(err){
-            console.log(err);
-            return;
-        }
-        return res.render('home', {
-            title: "Placement Cell",
-            students: students
+// module.exports.home = function(req, res){
+//     Student.find({}, function(err, students){
+//         if(err){
+//             console.log(err);
+//             return;
+//         }
+//         return res.render('home', {
+//             title: "Placement Cell",
+//             students: students
+//         });
+//     });
+// }
+
+module.exports.home = function (req, res) {
+    if (req.isAuthenticated()) {
+        Student.find({}, function (err, students) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            return res.render('home', {
+                title: "Placement Cell",
+                students: students
+            });
         });
-    });
+    } else {
+        return res.render('user_sign_in', {
+            title: "Sign In"
+        })
+    }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const passport = require('passport');
 
 const router = express.Router();
 const homeController = require('../controllers/home_controller')
@@ -6,6 +7,10 @@ const homeController = require('../controllers/home_controller')
 console.log('router loaded');
 
 // for any futher routes, access from here
+// use passport as a middleware to authenticate
+// router.get('/', passport.authenticate('local',
+//     {failureRedirect: '/users/sign-in'}
+//  ), homeController.home);
 router.get('/', homeController.home);
 
 router.use('/users', require('./users'));
